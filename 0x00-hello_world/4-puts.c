b39101bfeb93b2e9ce0d71b13af65b313a46a65a
root <liyu.beza.mek@gmail.com>
2023-03-02 07:08:15 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - this is programming for printing a line in puts function
+ * Return: 0 (good)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
